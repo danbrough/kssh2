2b037422c5b5fd6d59cb238a9b740a29f592e464
Dan Brough <dan@danbrough.org>
2026-09-10 15:48:24 -0600

    update

--- test/src/nativeMain/kotlin/demo/test/lib/jni.kt
+++ test/src/nativeMain/kotlin/demo/test/lib/jni.kt
@@ -1,5 +1,6 @@
 package demo.test.lib
 
+import demo.test.ssh2.cinterops.libssh2_init
 import kotlinx.cinterop.CPointer
 import platform.android.JNIEnvVar
 import platform.android.jclass
@@ -7,4 +8,7 @@ import platform.android.jclass
 @CName("Java_demo_test_lib_LibSSH2_initLib")
 fun ssh2InitLib(env: CPointer<JNIEnvVar>, clz: jclass) {
   println("Initializing libssh2 ..")
+  libssh2_init(0).also {
+    println("libssh2_init(0) returned $it")
+  }
 }
