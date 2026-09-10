cac27e2a93f70ba5490c686b7d3c8404d2500280
Dan Brough <dan@danbrough.org>
2026-09-10 10:51:44 -0600

    update

--- core/src/nativeMain/kotlin/io/github/danbrough/kssh2/lib/LibSSH2.native.kt
+++ core/src/nativeMain/kotlin/io/github/danbrough/kssh2/lib/LibSSH2.native.kt
@@ -46,6 +46,9 @@ import kotlinx.coroutines.flow.flow
 
 
 actual object LibSSH2 {
+
+  actual fun testJNI(){}
+  
   actual fun initLib() {
     kssh2_init(0).also {
       logNative.debug { "LibSSH2Native::initLib() kssh2_init() returned: $it" }
