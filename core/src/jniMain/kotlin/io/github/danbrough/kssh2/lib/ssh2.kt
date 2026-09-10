72e80eb596ac48c553ea41a7eb001d85db41c2d8
Dan Brough <dan@danbrough.org>
2026-09-10 13:31:16 -0600

    update

--- core/src/jniMain/kotlin/io/github/danbrough/kssh2/lib/ssh2.kt
+++ core/src/jniMain/kotlin/io/github/danbrough/kssh2/lib/ssh2.kt
@@ -2,24 +2,80 @@
 
 package io.github.danbrough.kssh2.lib
 
-import kotlinx.cinterop.COpaquePointer
 import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.CPointerVar
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.invoke
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.pointed
+import kotlinx.cinterop.ptr
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.staticCFunction
+import kotlinx.cinterop.value
 import org.danbrough.klog.logger
 import platform.android.JNIEnvVar
+import platform.android.JNINativeMethod
+import platform.android.JNI_ERR
+import platform.android.JNI_OK
+import platform.android.JNI_VERSION_1_6
+import platform.android.JavaVMVar
 import platform.android.jclass
+import platform.android.jint
 import platform.android.jlong
+
+
+
+import platform.posix.* // For pointer tracking if needed
 
 val log = logger("SSH2_JNI")
 
+/*@CName("JNI_OnLoad")
+fun jniOnLoad(vm: CPointer<JavaVMVar>, reserved: CPointer<out Any>?): jint {
+  //initRuntimeIfNeeded()
+
+  memScoped {
+    val envPtr = alloc<CPointerVar<JNIEnvVar>>()
+    val vmMethods = vm.pointed.pointed!!
+
+    // Retrieve the JNIEnv pointer from the JVM instance
+    if (vmMethods.GetEnv!!(vm, envPtr.ptr.reinterpret(), JNI_VERSION_1_6) != JNI_OK) {
+      return JNI_ERR
+    }
+
+    val env = envPtr.value!!
+    val envMethods = env.pointed.pointed!!
+
+    // 1. Locate your Java class
+    val className = "io/github/danbrough/kssh2/lib/LibSSH2"
+    val clazz = envMethods.FindClass!!(env, className.cstr.ptr) ?: return JNI_ERR
+
+    // 2. Register your native methods manually
+    val methods = allocArray<JNINativeMethod>(1)
+    methods[0].name = "initJNI".cstr.ptr
+    methods[0].signature = "()V".cstr.ptr // Change signature to match your actual Java method!
+    methods[0].fnPtr = staticCFunction(::initJNIImpl).reinterpret()
+
+    if (envMethods.RegisterNatives!!(env, clazz, methods, 1) < 0) {
+      return JNI_ERR
+    }
+  }
+
+  return JNI_VERSION_1_6
+}*/
 
 const val JNI_PREFIX = "Java_io_github_danbrough_kssh2_lib_LibSSH2"
 
 @CName("initJniBridge")
 fun initJniBridge() {
-    // Leave empty. This tells the compiler the file is a global runtime root.
+  // Leave empty. This tells the compiler the file is a global runtime root.
 }
 
-
+// A normal Kotlin native function matching the JNI signature
+fun initJNIImpl(env: CPointer<JNIEnvVar>, clz: jclass) {
+  log.info { "Hello from Kotlin/Native via Dynamic JNI!" }
+}
 /*@CName("Java_io_github_danbrough_kssh2_lib_LibSSH2_testJNI")
 fun ssh2TestJNI(env: CPointer<JNIEnvVar>, clz: jclass) {
   log.info { "testJNI worked!" }
